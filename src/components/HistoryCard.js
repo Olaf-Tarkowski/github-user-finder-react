ff7fc400ac8071b41775c69c61c4ad7931ee3412
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-03-05 22:50:14 +0100

    styling added

--- src/components/HistoryCard.js
+++ src/components/HistoryCard.js
@@ -1,19 +1,58 @@
 import React from "react";
+import styled from "styled-components";
 
-const HistoryCard = ({ handleSearchHistory, searchHistory}) => {
+const Container = styled.div`
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  background: #fff;
+`;
+
+const Title = styled.div`
+  padding-bottom: 10px;
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+const ButtonSection = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  font-family: "Roboto Condensed", sans-serif;
+  font-size: 16px;
+  line-height: 125%;
+  text-align: center;
+  text-transform: capitalize;
+  color: #ffffff;
+  background: #212529;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.5s;
+  :hover {
+    transform: scale(1.1);
+  }
+  :active {
+    background: #74c69d;
+  }
+`;
+
+const HistoryCard = ({ handleSearchHistory, searchHistory }) => {
   return (
-    <div>
-      <h3>Search History:</h3>
-      <ul>
+    <Container>
+      <Title>Search History:</Title>
+      <ButtonSection>
         {searchHistory.map((choice) => (
-          <li key={choice}>
-            <button onClick={() => handleSearchHistory(choice)}>
-              {choice}
-            </button>
-          </li>
+          <Button key={choice} onClick={() => handleSearchHistory(choice)}>
+            #{choice}
+          </Button>
         ))}
-      </ul>
-    </div>
+      </ButtonSection>
+    </Container>
   );
 };
 
