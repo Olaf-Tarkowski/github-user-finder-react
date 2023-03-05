677e392cc35bc7aba5be8eeba378760e74d9c35f
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-03-05 22:55:04 +0100

    add   max-width: 800px; for webside

--- src/components/ContentContainer.js
+++ src/components/ContentContainer.js
@@ -3,19 +3,24 @@ import SearchBar from "./SearchBar";
 import Card from "./Card";
 import HistoryCard from "./HistoryCard";
 import { useUserData } from "./useUserData";
+import styled from "styled-components";
+
+const Container = styled.main`
+  max-width: 800px;
+`;
 
 const ContentContainer = () => {
   const { changeUser, user, searchHistory, error } = useUserData();
 
   return (
-    <main>
+    <Container>
       <SearchBar changeUser={changeUser} error={error} />
       <Card user={user} />
       <HistoryCard
         searchHistory={searchHistory}
         handleSearchHistory={changeUser}
       />
-    </main>
+    </Container>
   );
 };
 export default ContentContainer;
